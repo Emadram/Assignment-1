f510c2ac831766a05d495cd9cad4e8c80dffbf76
Emadram <refemad@outlook.com>
2025-03-25 10:11:56 +0200

    student_management.py lines: 4-5 | Type: Replacement | Reason: Student Class attirbutes Doc

--- student_management.py
+++ student_management.py
@@ -2,6 +2,12 @@ class Student:
     """
     #-Student class with private attribtutes.
     #-With getter and setter
+    @param __student_number: Student number which is used as a unique identifier @Type = Str
+    @param __first_name: Student first name @Type = Str
+    @param __last_name: Student last name @Type = Str
+    @param __date_of_birth: Student date of birth @Type = Str
+    @param __country_of_birth: Student country of birth @Type = Str
+    @param __sex: Student sex (Male/Female) @Type = Str
     """
     def __init__(self, student_number, first_name
         last_name, date_of_birth, country_of_birth, sex):
