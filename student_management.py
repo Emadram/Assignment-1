5b5b42f3c3a2f997ba44dd3010fae50577c7c381
Emadram <refemad@outlook.com>
2025-03-25 10:06:29 +0200

    student_management.py lines: 2-3 | Type: Replacement | Reason: Student Class Doc

--- student_management.py
+++ student_management.py
@@ -1,5 +1,7 @@
 class Student:
     """
+    #-Student class with private attribtutes.
+    #-With getter and setter
     """
     def __init__(self, student_number, first_name
         last_name, date_of_birth, country_of_birth, sex):
