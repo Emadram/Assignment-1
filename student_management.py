02f815735a5f571ddc7150e61788fd451d8599ee
Emadram <refemad@outlook.com>
2025-03-25 10:05:05 +0200

    student_management.py lines: 3-4 | Type: Replacement | Reason: Student Class init

--- student_management.py
+++ student_management.py
@@ -1,6 +1,14 @@
 class Student:
     """
     """
+    def __init__(self, student_number, first_name
+        last_name, date_of_birth, country_of_birth, sex):
+    self.__student_number = student_number
+    self.__first_name = first_name
+    self.__last_name = last_name
+    self.__date_of_birth = date_of_birth
+    self.__country_of_birth = country_of_birth
+    self.__sex = sex
 
 
 
