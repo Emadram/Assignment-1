73a5c7a23e6d2dba8d8324d57b39099cec8b5a2a
Emadram <refemad@outlook.com>
2025-03-25 15:04:41 +0200

    student_management.py lines: 30-30 37-37 44-44 51-51 58-58 65-65 72-72 83-83 90-90 97-97 104-104 111-111 118-118 | Type: Replacement | Reason: changed methods to camelCase

--- student_management.py
+++ student_management.py
@@ -27,49 +27,49 @@ class Student:
     #-Student number getter
     @returns students number @Type = Str
     """
-    def get_student_number(self):
+    def getStudentNumber(self):
         return self.__student_number
 
     """
     #-Student first_name getter
     @returns first name of student @Type = Str
     """
-    def get_first_name(self):
+    def getFirstName(self):
         return self.__first_name
 
     """
     #-Student last_name getter
     @returns last name of student @Type = Str
     """
-    def get_last_name(self):
+    def getLastName(self):
         return self.__last_name
 
     """
     #-Student date of birth getter
     @returns students date of birth @Type = Str
     """
-    def get_date_of_birth(self):
+    def getDateOfBirth(self):
         return self.__date_of_birth
 
     """
     #-Student country of birth getter
     @returns students country of birth @Type = Str
     """
-    def get_country_of_birth(self):
+    def getCountryOfBirth(self):
         return self.__country_of_birth
 
     """
     #-Student sex getter
     @returns students sex @Type = Str
     """
-    def get_sex(self):
+    def getSex(self):
         return self.__sex
 
     """
     #-Student age getter
     @returns students age @Type = Str
     """
-    def get_age(self):
+    def getAge(self):
         current_year = datetime.date.today().year
         return  current_year - self.__date_of_birth
 
@@ -80,42 +80,42 @@ class Student:
     #-Student number setter
     @param student_number = passed student number @Type = Str
     """
-    def set_student_number(self, student_number):
+    def setStudentNumber(self, student_number):
         self.__student_number = student_number
 
     """
     #-Student first_name setter
     @param first_name = passed student first_name @Type = Str
     """
-    def set_first_name(self, first_name):
+    def setFirstName(self, first_name):
         self.__first_name = first_name
 
     """
     #-Student last_name setter
     @param last_name = passed student last_name @Type = Str
     """
-    def set_last_name(self, last_name):
+    def setLastName(self, last_name):
         self.__last_name = last_name
 
     """
     #-Student date_of_birth setter
     @param date_of_birth = passed student date_of_birth @Type = Str
     """
-    def set_date_of_birth(self, date_of_birth):
+    def setDateOfBirth(self, date_of_birth):
         self.__date_of_birth = date.datetime.strptime(date_of_birth, "%Y-%m-%d")
 
     """
     #-Student country_of_birth setter
     @param country_of_birth = passed student country_of_birth @Type = Str
     """
-    def set_country_of_birth(self, country_of_birth):
+    def setCountryOfBirth(self, country_of_birth):
         self.__country_of_birth = country_of_birth
 
     """
     #-Student sex setter
     @param sex = passed student sex @Type = Str
     """
-    def set_sex(self, sex):
+    def setSex(self, sex):
         self.sex = sex
 
 def main():
