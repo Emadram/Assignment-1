51c46883ebc8804dc23caf560ac638ce24531ad5
Ali <alighrehgozloo@gmail.com>
2025-04-02 04:15:32 +0300

    functions are done, comments were added and only the menu is left, some tweaks were made due to compile error

--- Assign_2/LexicalAnalyzerAssign_2.py
+++ Assign_2/LexicalAnalyzerAssign_2.py
@@ -1,75 +1,122 @@
+import re
 
 class TokenBuffer:
-	"""
+    """
+    Represents a token with its type and value.
+    """
+    def __init__(self, token_type, token_value):
+        self.token_type = token_type
+        self.token_value = token_value
 
-	"""
-
-	def __init__(self, token_type, token_value):
-		self.token_type = token_type
-		self.token_value = token_value
-
-
-	def __str__(self):
-		return f"<{self.token_type}, {self.value}>"
-
+    def __str__(self):
+        return f"<{self.token_type}, {self.token_value}>"
 
 class TokenLexicalAnalyzer:
-	"""
-	
-	"""
+    """
+    A lexical analyzer that tokenizes identifiers, numbers, and logical/bitwise operators.
+    """
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file = None  # File reference
+        self.buffer = ""  # File content buffer
+        self.pointer_position = 0  # Pointer for traversal
+        self.symbol_buffer = []  # Symbol table
 
-	def __init__(self, file_name):
-		self.file_name = file_name
-		self.file = None #Init value of file
-		self.buffer = "" #Init value of buffer
-		self.pointer_position = 0 #Init place for the pointer to look at in the file
-		self.symbol_buffer = [] #Init of symbol buffer
+    def openFile(self):
+        """Opens the input file and reads its contents into a buffer."""
+        try:
+            self.file = open(self.file_name, 'r')
+            self.buffer = self.file.read()
+            return True
+        except FileNotFoundError:
+            print(f"Error: No such file found {self.file_name}")
+            return False
 
+    def closeFile(self):
+        """Closes the input file if it's open."""
+        if self.file:
+            self.file.close()
 
-	def openFile(self):#TODO PUT COMMENTS -> EMAD
+    def ignoreWhiteSpace(self):
+        """Moves the pointer past whitespace characters."""
+        while self.pointer_position < len(self.buffer) and self.buffer[self.pointer_position].isspace():
+            self.pointer_position += 1
 
-  		try:
-  			self.file = open(self.file_name, 'r')
-  			self.buffer = self.file.read()
-  			return True
-
-  		except FileNotFoundError:
-  			print(f"Error: No such File found {self.file_name}")
-  			return False
-
-	def closeFile(self):#TODO PUT COMMENTS -> EMAD
-
-  		if self.file == True:
-  			self.file.close()
-
-	def ignoreWhiteSpace(self):#TODO PUT COMMENTS -> EMAD
-  		while self.pointer_position < len(self.buffer) and self.buffer[self.pointer_position] == " ":
-  			self.pointer_position += 1
-
-
-  	#TODO def lex(self)
-
-  	#TODO def checkNumber(self)
-
-  	#TODO def checkIdentifier(self)
-
-  	#TODO def checkAnd(self)
-
-  	#TODO def checkOr(self)
-
-  	#TODO def getSymbolBuffer
-
-
+    def lex(self):
+        """Processes the next token in the buffer."""
+        self.ignoreWhiteSpace()
+        if self.pointer_position >= len(self.buffer):
+            return None
+        
+        char = self.buffer[self.pointer_position]
+        
+        if char.isdigit() or (char == '-' and self.pointer_position + 1 < len(self.buffer) and self.buffer[self.pointer_position + 1].isdigit()):
+            return self.checkNumber()
+        elif char.isalpha() or char == '_':
+            return self.checkIdentifier()
+        elif char == '&':
+            return self.checkAnd()
+        elif char == '|':
+            return self.checkOr()
+        else:
+            self.pointer_position += 1
+            return TokenBuffer("ERROR", f'Unrecognized token: {char}')
+    
+    def checkNumber(self):
+        """Extracts an integer or floating point number from the buffer."""
+        num_pattern = re.compile(r'-?\d+\.?\d*')
+        match = num_pattern.match(self.buffer, self.pointer_position)
+        if match:
+            num_str = match.group()
+            temp_position = self.pointer_position + len(num_str)
+            # Check if the number is followed by letters (invalid token)
+            invalid_pattern = re.compile(r'-?\d+\.?\d*[a-zA-Z]+')
+            invalid_match = invalid_pattern.match(self.buffer, self.pointer_position)
+            if invalid_match:
+                invalid_token = invalid_match.group()
+                self.pointer_position += len(invalid_token)
+                return TokenBuffer("ERROR", f'"{invalid_token}"')
+            # Valid number
+            self.pointer_position = temp_position
+            if '.' in num_str:
+                return TokenBuffer("FLOAT", float(num_str))
+            return TokenBuffer("INTEGER", int(num_str))
+        return TokenBuffer("ERROR", "Invalid number format")
+    
+    def checkIdentifier(self):
+        """Extracts an identifier from the buffer."""
+        id_pattern = re.compile(r'[a-zA-Z_][a-zA-Z0-9_]*')
+        match = id_pattern.match(self.buffer, self.pointer_position)
+        if match:
+            identifier = match.group()
+            self.pointer_position += len(identifier)
+            if identifier not in self.symbol_buffer:
+                self.symbol_buffer.append(identifier)
+            return TokenBuffer("ID", self.symbol_buffer.index(identifier))
+        return TokenBuffer("ERROR", "Invalid identifier format")
+    
+    def checkAnd(self):
+        """Handles bitwise and logical AND operators."""
+        if self.pointer_position + 1 < len(self.buffer) and self.buffer[self.pointer_position + 1] == '&':
+            self.pointer_position += 2
+            return TokenBuffer("LOGICAL_AND", "nothing")
+        self.pointer_position += 1
+        return TokenBuffer("BITWISE_AND", "nothing")
+    
+    def checkOr(self):
+        """Handles bitwise and logical OR operators."""
+        if self.pointer_position + 1 < len(self.buffer) and self.buffer[self.pointer_position + 1] == '|':
+            self.pointer_position += 2
+            return TokenBuffer("LOGICAL_OR", "nothing")
+        self.pointer_position += 1
+        return TokenBuffer("BITWISE_OR", "nothing")
+    
+    def getSymbolBuffer(self):
+        """Returns the symbol table."""
+        return self.symbol_buffer
 
 def main():
- 	yield #TODO
- 	#USER INTERFACE AND INPUT CODE
-
-
-
-
-
-
+#TODO BY BESTE
 
 if __name__ == "__main__":
-	main()
+    main()
