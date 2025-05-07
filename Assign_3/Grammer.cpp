b51b82994f75337f65beb567de88728155007343
Emadram <refemad@outlook.com>
2025-05-07 11:20:17 +0300

    Created and added the respected function templates

--- Assign_3/Grammer.cpp
+++ Assign_3/Grammer.cpp
@@ -1,13 +1,13 @@
 #include "ParserApplicationHeader.h"
 
-// E → T R
+// E → T R TODO
 void E();
 
-// R → + T R | - T R | ε
+// R → + T R | - T R | ε TODO
 void R();
 
-// T → F S
+// T → F S TODO
 void T();
 
-// S → * F S | / F S | ε
+// S → * F S | / F S | ε TODO
 void S();
