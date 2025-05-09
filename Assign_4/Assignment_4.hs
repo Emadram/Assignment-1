99e5a0fdd7d13dc5855a174623b73b2dd2e462bb
Emadram <refemad@outlook.com>
2025-05-09 11:29:17 +0300

    Updated

--- Assign_4/Assignment_4.hs
+++ Assign_4/Assignment_4.hs
@@ -1,2 +1,34 @@
+-- Problem 1: Get numbers divisible by 3
+--TODO
+
+-- Problem 2: Get numbers divisible by 2
+--TODO
+
+-- Problem 3: Count occurrences in a list
+--TODO
+
+-- Problem 4: Triple all values in a list
+--TODO
+
+-- Problem 5: Sort three numbers
+--TODO
+
+-- Problem 6: Get a slice of the list
+--TODO
+
+-- Problem 7: Get the nth element (recursive way)
+--TODO
+
+-- Problem 8: Get the mth element (using library functions)
+--TODO
+
+-- Problem 9: Add corresponding elements from two lists
+--TODO
+
+-- Problem 10: Convert numeric grade to letter grade
+--TODO
+
+--TEST SECTION
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = do
+            
